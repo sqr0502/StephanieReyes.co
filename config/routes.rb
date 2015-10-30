7e89ca6c2bf678ddfd415840d2406b16cd45d358
Estefania <sqr0502@gmail.com>
2015-10-29 21:26:03 -0400

    Create portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'portfolio#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
